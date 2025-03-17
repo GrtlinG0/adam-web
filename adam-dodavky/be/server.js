c19110da5d26ed0d06ea025cb4648a4c3b47786a
GrtlinG <108225633+GrtlinG0@users.noreply.github.com>
2025-03-17 16:21:00 +0100

    added env variable for mongo in server.js

--- adam-dodavky/be/server.js
+++ adam-dodavky/be/server.js
@@ -1,23 +1,14 @@
-const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
-const timelineRoutes = require('./routes/timeline');
-
+const express = require('express');
 const app = express();
-const port = 3000;
 
-app.use(cors());
-app.use(express.json());
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/adam-dodavky';
 
-// Připojení k MongoDB
-mongoose.connect('mongodb://localhost/adam-dodavky', {
+mongoose.connect(mongoURI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('Připojeno k MongoDB'));
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Připojeno k MongoDB'))
+  .catch(err => console.error('Chyba připojení k MongoDB:', err));
 
-// Routy
-app.use('/api/timeline', timelineRoutes);
-
-app.listen(port, () => {
-  console.log(`Server běží na http://localhost:${port}`);
-});
+app.listen(3000, () => console.log('Server běží na http://localhost:3000'));
